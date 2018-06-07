e97d3e83e5514f4b92217b0170d6175d00a65e88
tsupio <t.supiot@iia-laval.fr>
2018-06-07 09:51:41 +0200

    Before / After test

--- src/test/java/com/cours/tp1/ApplicationTest.java
+++ src/test/java/com/cours/tp1/ApplicationTest.java
@@ -1,9 +1,23 @@
 package com.cours.tp1;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ApplicationTest {
+
+    @Before
+    // S'exécute AVANT chaque test
+    public void setup(){
+        System.out.println("Before test");
+    }
+
+    @After
+    // S'exécute APRES chaque test
+    public void teardown(){
+        System.out.println("After test");
+    }
 
     @Test
     public void testAdd(){
