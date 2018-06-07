821a522a1fe46d428c43b17e480ef441356673fc
tsupio <t.supiot@iia-laval.fr>
2018-06-07 10:40:47 +0200

    javadoc

--- src/main/java/com/cours/tp1/Application.java
+++ src/main/java/com/cours/tp1/Application.java
@@ -1,20 +1,43 @@
+/*
+ * Copyright (c) 2018 Théodore SUPIOT <theodore.supiot@gmail.com> Allright reserved
+ */
+
 package com.cours.tp1;
 
 import org.joda.time.DateTime;
 
+/**
+ * My main Application.
+ * @author Théodore Supiot
+ * @version 1.0
+ */
 public class Application {
+    /**
+     * Entry point of the application.
+     * @param args Argument for CLI.
+     */
     public static void main(String[] args) {
-        // write your code here
         System.out.println("Hello World !!");
-
         DateTime date = DateTime.now();
         System.out.println(date.toString("dd/MM/YYYY HH:mm"));
     }
 
+    /**
+     * Add values and return the result.
+     * @param i first value.
+     * @param j second value.
+     * @return result of addition
+     */
     public static int add(int i, int j) {
         return i + j;
     }
 
+    /**
+     * Subtract values and return the result.
+     * @param i first value
+     * @param j second value
+     * @return result of subtraction
+     */
     public static int subtract(int i, int j) {
         return i - j;
     }
---
+++ src/main/java/com/cours/tp1/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2018 Théodore SUPIOT <theodore.supiot@gmail.com> Allright reserved
+ */
+
+/**
+ * Package TP1 du cours méthodologie Agile - Master 2 - 2018
+ */
+package com.cours.tp1;
